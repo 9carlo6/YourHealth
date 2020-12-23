6d23ba17be8e32cc3762363148c919ffde82e989
9carlo6 <p.carl@hotmail.it>
2020-12-23 18:49:31 +0100

    miglioramento generale

--- YourHealth/ContentView.swift
+++ YourHealth/ContentView.swift
@@ -7,56 +7,126 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
+    
+    @State var tabColor: UIColor = UIColor.init(red: 255/255, green: 226/255, blue: 226/255,alpha: 0.0)
+    @State var navColor: Color = Color.init(red: 255/255, green: 240/255, blue: 240/255)
+    
+    
     var body: some View{
-        
+
         ZStack{
             TabView{
                 NavigationView{
-                    Form{
-                        Section{
-                            Text("Hello World")
-                        }
+                    ZStack{
+                        navColor.edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                        Text("Dashboard")
+                            .navigationBarTitle(Text("Dashboard"))
+                            .navigationBarItems(trailing:
+                                HStack {
+                                    Button(action: {
+                                        print("ciao")
+                                    }, label:{
+                                        Image(systemName: "trash")
+                                    })
+                                }
+                            )
                     }
-                    .navigationBarTitle(Text("Dashboard"))
-                    .navigationBarItems(trailing:
-                        HStack {
-                            Button(action: {
-                                print("ciao")
-                            }, label:{
-                                Image(systemName: "trash")
-                            })
-                        }
-                    )
                 }
                 .tabItem {
                     Image(systemName: "doc.circle.fill")
                     Text("Dashboard")
                 }
                 
-                Text("schedule")
+                NavigationView{
+                    ZStack{
+                        navColor.edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                        Text("Schedule")
+                            .navigationBarTitle(Text("Schedule"))
+                            .navigationBarItems(trailing:
+                                HStack {
+                                    Button(action: {
+                                        print("Schedule")
+                                    }, label:{
+                                        Image(systemName: "trash")
+                                    })
+                                }
+                            )
+                    }
+                }
                     .tabItem {
                         Image(systemName: "calendar.circle.fill")
                         Text("Schedule")
                     }
-                Text("chats")
+                NavigationView{
+                    ZStack{
+                        navColor.edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                        Text("Chats")
+                            .navigationBarTitle(Text("Chats"))
+                            .navigationBarItems(trailing:
+                                HStack {
+                                    Button(action: {
+                                        print("Chats")
+                                    }, label:{
+                                        Image(systemName: "trash")
+                                    })
+                                }
+                            )
+                    }
+                }
                     .tabItem {
                         Image(systemName: "message.circle.fill")
                         Text("Chats")
                     }
-                Text("specialist")
+                NavigationView{
+                    ZStack{
+                        navColor.edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                        Text("Specialists")
+                            .navigationBarTitle(Text("Specialists"))
+                            .navigationBarItems(trailing:
+                                HStack {
+                                    Button(action: {
+                                        print("Specialists")
+                                    }, label:{
+                                        Image(systemName: "trash")
+                                    })
+                                }
+                            )
+                    }
+                }
                     .tabItem {
                         Image(systemName: "asterisk.circle.fill")
                         Text("Specialist")
                     }
-                Text("settings")
+                NavigationView{
+                    ZStack{
+                        navColor.edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                        Text("Settings")
+                            .navigationBarTitle(Text("Settings"))
+                            .navigationBarItems(trailing:
+                                HStack {
+                                    Button(action: {
+                                        print("Settings")
+                                    }, label:{
+                                        Image(systemName: "trash")
+                                    })
+                                }
+                            )
+                    }
+                }
                     .tabItem {
                         Image(systemName: "gearshape.fill")
                         Text("Settings")
                     }
             }
+            .accentColor(.black)
+            .onAppear() {
+                UITabBar.appearance().barTintColor = tabColor
+                   }
             
-        }.background(Color.black.opacity(0.5))
+        }
     }
 }
 
