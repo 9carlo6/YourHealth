eb7ecc6ef7148b63747dff465b833f44e712a7f6
EddyDevProject <e.bavaro@studenti.unisa.it>
2020-12-24 14:30:29 +0100

    Login Page Test

---
+++ YourHealth/LoginPage.swift
@@ -0,0 +1,20 @@
+//
+//  LoginPage.swift
+//  YourHealth
+//
+//  Created by bavaroedoardo on 24/12/2020.
+//
+
+import SwiftUI
+
+struct LoginPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginPage_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginPage()
+    }
+}
